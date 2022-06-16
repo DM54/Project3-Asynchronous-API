5585786d34a3fe215c0a0e5e121d84a7b04295da
Dyana Monroy <dyana.monroy17@live.com>
2022-06-16 17:19:10 -0400

    created apikey and server localhost

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,4 +1,8 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = '14d242caefcc16d1d730d8cb6fec4d96&units=imperial';
+const baseURL = `api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid=${apiKey}`;
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
